3deebbaa3ef29bdc784e0709192d393d937e41ad
Jorge Molina <jorge.e.molina@hotmail.com>
2021-12-24 11:32:06 -0600

    added localStorage to script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+var btn9 = $("#btn9");
+
 var currentDay = document.getElementById("currentDay");
 setInterval(() => {
     var now = moment();
     var actualTime = now.format("MMMM Do YYYY, h:mm:ssA");
     currentDay.textContent = actualTime;
 }, 1000);
+var buttonEl = document.querySelector("#btn9");
+buttonEl.addEventListener("click", function(saveTasks) {
+    localStorage.setItem("#btn9", JSON.stringify(btn9));
+console.log(localStorage);
+});
+
+//save item to local storage
+var saveTasks = function() {
+localStorage.setItem("#btn9", JSON.stringify(btn9));
+console.log(localStorage);
+}
